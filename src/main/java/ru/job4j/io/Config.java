80d6639562af0cf3856ea96e43d794909b4ef5d4
Kirill <npabllla@gmail.com>
2021-02-02 00:33:36 +0300

    1. Читаем файл конфигурации [#451240]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            List<String> list = read.lines().collect(Collectors.toList());
+            for (String st : list) {
+                if (!st.contains("#") && !st.equals("")) {
+                    String[] temp = st.split("=");
+                    values.put(temp[0], temp[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.dialect"),
+                is("org.hibernate.dialect.PostgreSQLDialect")
+        );
+        assertThat(
+                config.value("hibernate.connection.url"),
+                is("jdbc:postgresql://127.0.0.1:5432/trackstudio")
+        );
+        assertThat(
+                config.value("hibernate.connection.driver_class"),
+                is("org.postgresql.Driver")
+        );
+        assertThat(
+                config.value("hibernate.connection.username"),
+                is("postgres")
+        );
+        assertThat(
+                config.value("hibernate.connection.password"),
+                is("password")
+        );
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("hibernate.dialect"),
+                is("org.hibernate.dialect.PostgreSQLDialect")
+        );
+        assertThat(
+                config.value("hibernate.connection.url"),
+                is("jdbc:postgresql://127.0.0.1:5432/trackstudio")
+        );
+        assertThat(
+                config.value("hibernate.connection.driver_class"),
+                is("org.postgresql.Driver")
+        );
+        assertThat(
+                config.value("hibernate.connection.username"),
+                is("postgres")
+        );
+        assertThat(
+                config.value("hibernate.connection.password"),
+                is("password")
+        );
+    }
+}
