1786e7c86484f143e0efa92c3c867cc23eee809c
Kirill <npabllla@gmail.com>
2021-02-03 13:26:43 +0300

    3.0. Тестирование IO [#451232]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -19,12 +19,12 @@ public class AnalizyTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01\n"
-                    + "500 10:57:01\n"
-                    + "400 10:58:01\n"
-                    + "200 10:59:01\n"
-                    + "500 11:01:02\n"
-                    + "200 11:02:02");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
         analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
@@ -40,12 +40,12 @@ public class AnalizyTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01\n"
-                    + "500 10:57:01\n"
-                    + "400 10:58:01\n"
-                    + "500 10:59:01\n"
-                    + "500 11:01:02\n"
-                    + "200 11:02:02");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
         analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
@@ -61,12 +61,12 @@ public class AnalizyTest {
         File source = folder.newFile("source.txt");
         File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 \n"
-                    + "500 10:57:01\n"
-                    + "400 10:58:01\n"
-                    + "500 10:59:01\n"
-                    + "500 11:01:02\n"
-                    + "200 11:02:02");
+            out.println("200 ");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
         analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
     }
