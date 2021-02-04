0d55f0d2136060cac87a8bbde793b234bf009823
Kirill <npabllla@gmail.com>
2021-02-04 20:52:41 +0300

    6. Кодировка. [#451243]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -21,45 +21,53 @@ public class ConsoleChat {
 
     public void run() {
         Scanner in = new Scanner(System.in);
-        try (BufferedWriter br = new BufferedWriter(new FileWriter(this.path))) {
-            String userPhrase = in.nextLine();
-            String botAnswer = getBotAnswers(this.botAnswers);
-            System.out.println(botAnswer);
-            br.write("User: " + userPhrase + System.lineSeparator());
-            br.write("Bot: " + botAnswer + System.lineSeparator());
-            while (!userPhrase.equals(OUT)) {
-                botAnswer = getBotAnswers(this.botAnswers);
-                userPhrase = in.nextLine();
-                if (userPhrase.equals(STOP)) {
-                    br.write("User: " + userPhrase + System.lineSeparator());
-                    while (!userPhrase.equals(CONTINUE)) {
-                        userPhrase = in.nextLine();
-                        if (!userPhrase.equals(CONTINUE)) {
-                            br.write("User: " + userPhrase + System.lineSeparator());
-                        }
+        List<String> botAns = getBotAnswers(this.botAnswers);
+        Random index = new Random();
+        List<String> log = new ArrayList<>();
+        String userPhrase = in.nextLine();
+        String botAnswer = botAns.get(index.nextInt(botAns.size()));
+        System.out.println(botAnswer);
+        log.add("User: " + userPhrase + System.lineSeparator());
+        log.add("Bot: " + botAnswer + System.lineSeparator());
+        while (!userPhrase.equals(OUT)) {
+            botAnswer = botAns.get(index.nextInt(botAns.size()));
+            userPhrase = in.nextLine();
+            if (userPhrase.equals(STOP)) {
+                log.add("User: " + userPhrase + System.lineSeparator());
+                while (!userPhrase.equals(CONTINUE)) {
+                    userPhrase = in.nextLine();
+                    if (!userPhrase.equals(CONTINUE)) {
+                        log.add("User: " + userPhrase + System.lineSeparator());
                     }
                 }
-                br.write("User: " + userPhrase + System.lineSeparator());
-                if (!userPhrase.equals(OUT)) {
-                    System.out.println(botAnswer);
-                    br.write("Bot: " + botAnswer + System.lineSeparator());
-                }
             }
+            log.add("User: " + userPhrase + System.lineSeparator());
+            if (!userPhrase.equals(OUT)) {
+                System.out.println(botAnswer);
+                log.add("Bot: " + botAnswer + System.lineSeparator());
+            }
+        }
+        writeLog(log, this.path);
+    }
 
+    private static void writeLog(List<String> log, String path) {
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(path))) {
+            for (String st : log) {
+                br.write(st);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String getBotAnswers(String path) throws IOException {
+    private static List<String> getBotAnswers(String path) {
         List<String> answers = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
             answers = in.lines().collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Random index = new Random();
-        return answers.get(index.nextInt(answers.size()));
+        return answers;
     }
 
     public static void main(String[] args) {
