0d6186e259e85232c38a6f8d7945065c21ba939a
Kirill <npabllla@gmail.com>
2021-02-04 16:55:59 +0300

    4. JAXB. Преобразование XML в POJO. [#451249]

--- src/main/java/ru/job4j/serialization/xml/Car.java
+++ src/main/java/ru/job4j/serialization/xml/Car.java
@@ -1,13 +1,33 @@
 package ru.job4j.serialization.xml;
 
+import java.io.StringReader;
 import java.util.Arrays;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
 
+@XmlRootElement(name = "car")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Car {
-    private final int capacity;
-    private final String brand;
-    private final boolean presence;
-    private final Engine engine;
-    private final String[] colors;
+
+    @XmlAttribute
+    private int capacity;
+
+    @XmlAttribute
+    private String brand;
+
+    @XmlAttribute
+    private boolean presence;
+
+    private Engine engine;
+    private String[] colors;
+
+    public Car() {
+
+    }
 
     public Car(int capacity, String brand, boolean presence, Engine engine, String[] colors) {
         this.capacity = capacity;
@@ -29,7 +49,23 @@ public class Car {
                 + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         final  Car car = new Car(5, "someBrand1", true, new Engine(147.8), new String[]{"Black", "White", "Red"});
+        JAXBContext context = JAXBContext.newInstance(Car.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(car, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Car result = (Car) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
--- src/main/java/ru/job4j/serialization/xml/Engine.java
+++ src/main/java/ru/job4j/serialization/xml/Engine.java
@@ -1,7 +1,17 @@
 package ru.job4j.serialization.xml;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlElement(value = "engine")
 public class Engine {
+
+    @XmlAttribute
     private double power;
+
+    public Engine() {
+
+    }
 
     public Engine(double power) {
         this.power = power;
