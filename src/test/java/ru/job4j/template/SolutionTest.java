77ea432858c3becc4a93273170b6076077f29b48
Kirill <npabllla@gmail.com>
2021-02-27 15:57:11 +0300

    3. Шаблонизатор. [#451261]

---
+++ src/test/java/ru/job4j/template/SolutionTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class SolutionTest {
+    @Test
+    public void whenAllRight() {
+        Solution solution = new Solution();
+        assertThat(solution.produce("I am a ${name}, Who are ${subject}?", Map.of("name", "Petr", "subject", "you")), is("I am a Petr, Who are you?"));
+    }
+
+    @Test(expected = IOException.class)
+    public void whenNotEnoughKeys() {
+        Solution solution = new Solution();
+        solution.produce("I am a ${name}, Who are ${subject}? ${emotion}", Map.of("name", "Petr", "subject", "you"));
+    }
+
+    @Test(expected = IOException.class)
+    public void whenToManyKeys() {
+        Solution solution = new Solution();
+        solution.produce("I am a ${name}, Who are ${subject}?", Map.of("name", "Petr", "subject", "you", "emotion", "Nice to meet u"));
+    }
+}
